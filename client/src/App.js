b50a59a4a10580790b012dac5998039cdc901f54
phebems <phebeshekoni1@hotmail.co.uk>
2022-02-02 00:32:00 +0000

    spotify cliend id and secret

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
+import { response } from "express";
 import React, { useState, useEffect } from "react";
 
 // SERVICES THAT CALL OUR API ENDPOINTS
-import { getAllProfiles } from "./services/profileService";
+import { getAllProfiles, getAuth, myReccomendations } from "./services/profileService";
 
 function App() {
   const [profiles, setProfiles] = useState(null);
+  const [accessToken, setAccessToken] = useState(null);
+  const[reccomendations, setReccomendations]=useState(null);
 
   useEffect(() => {
     async function getProfiles() {
@@ -17,6 +20,40 @@ function App() {
     getProfiles();
   }, [profiles]);
 
+  useEffect(() => {
+    async function getToken() {
+      if (!accessToken) {
+        const response = await getAuth();
+        setAccessToken(response);
+      }
+    }
+
+    getToken();
+  }, [accessToken]);
+
+  useEffect(() => {
+    async function getReccomendations(accessToken) {
+      if (accessToken) {
+        const response = await myReccomendations(accessToken);
+        setReccomendations(response);
+      }
+    }
+    getReccomendations();
+    console.log(reccomendations)
+  }, [accessToken])
+
+  // async function getReccomendations(accessToken) {
+  //   const url ='https://api.spotify.com/v1/recommendations/available-genre-seeds'
+  //   const results = await fetch(url).then((res) => res.json());
+  //   if (!results.error) {
+  //     setReccomendations(results);
+  //     //results.results.filter((item) => !basketIds.includes(item.trackId))
+      
+  //   }
+  // }
+
+  
+  
   const renderProfile = (user) => {
     return (
       <li key={user._id}>
@@ -28,9 +65,11 @@ function App() {
       </li>
     );
   };
+  console.log()
 
   return (
     <div>
+      {/* <p>{reccomendations.map((track)=>track.name)}</p> */}
       <ul>
         {profiles && profiles.length > 0 ? (
           profiles.map((profile) => renderProfile(profile))
--- client/src/services/profileService.js
+++ client/src/services/profileService.js
@@ -1,6 +1,8 @@
 // You can create all the fetches to your own APIs and externals APIs here
 // This example fetch is specifically for our Profile API and is why the file is called profileService.js
 import axios from "axios";
+import { Buffer } from "buffer";
+const qs = require("qs");
 
 const getAllProfiles = async () => {
   const response = await axios.get(`/api/profile`);
@@ -8,5 +10,36 @@ const getAllProfiles = async () => {
   return response.data || [];
 };
 
+const client_id = process.env.spotify_client_id;
+const client_secret = process.env.spotify_client_secret;
+const auth_token = 'YmQ0NjFiN2UwNDRmNGU5NTgzYzU0ZWUzMDcwMTFlNDc6Njk1MTgzNGM5OWVlNGQ0YzkzNzgyOTY4YjEwOTQ4ZDE='
+//Buffer.from(client_id + ":" + client_secret).toString("base64");
+const getAuth = async () => {
+  const data = qs.stringify({ grant_type: "client_credentials" });
+  //console.log(auth_token);
+  const response = await axios.post(
+    "https://accounts.spotify.com/api/token",
+    data,
+    {
+      headers: {
+        Authorization: `Basic ${auth_token}`,
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    }
+  );
+  return response.data.access_token;
+};
+
+const myReccomendations = async (accessToken) => {
+  console.log(accessToken)
+  const response = await axios.get('https://api.spotify.com/v1/recommendations?limit=10&market=ES&seed_artists=23zg3TcAtWQy7J6upgbUnj&seed_genres=r-n-b',{
+    headers: {
+      Authorization:`Bearer ${accessToken}`
+    },
+  })
+  console.log(response.data.tracks)
+  return response.data
+}
+
 // All of the endpoints in this file can be exported below
-export { getAllProfiles };
+export { getAllProfiles, getAuth, myReccomendations };
