ad845c960b7253c5d10b0fdd72740111cf393d78
phebems <phebeshekoni1@hotmail.co.uk>
2022-02-20 06:42:06 +0000

    get artist id

--- client/src/components/Artist.js
+++ client/src/components/Artist.js
@@ -1,13 +1,23 @@
-function Artist({artist}) {
-    const {id, images,name} = artist
-    return (
-        <div className="artist">
-      {images[2]? <img src={images[2].url} alt={name + 'profile picture'} /> : <img src="https://cdn-icons-png.flaticon.com/512/64/64572.png"/>}
+function Artist({ artist }) {
+  const { id, images, name } = artist;
+  const getId = (trackId) => {
+    console.log(trackId);
+  };
+  return (
+    <div className="artist" onClick={() => getId(id)}>
+      {images[2] ? (
+        <img src={images[2].url} alt={name + "profile"} />
+      ) : (
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/64/64572.png"
+          alt="default avi"
+        />
+      )}
       <div>
         <h3>{name}</h3>
       </div>
     </div>
-    );
+  );
 }
 
 export default Artist;
