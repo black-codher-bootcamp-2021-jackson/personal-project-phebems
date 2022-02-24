a0cfe58d9a4e0be9bb139eaa5acabbbe261e97d6
phebems <phebeshekoni1@hotmail.co.uk>
2022-02-24 23:22:09 +0000

    modal position and styling

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,9 +1,22 @@
 import React from "react";
 
 const Modal = ({ showModal, setShowModal }) => {
-  return <>{showModal ? <div className="modal">Modal
-  <button onClick={()=>setShowModal(prev => !prev)}>x</button>
-  </div> : null}</>;
+  return (
+    <>
+      {showModal ? (
+        <div className="modal">
+          <div className="closeButton">
+            <button
+              className="closeModal"
+              onClick={() => setShowModal((prev) => !prev)}
+            >
+              x
+            </button>
+          </div>
+        </div>
+      ) : null}
+    </>
+  );
 };
 
 export default Modal;
